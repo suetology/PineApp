c75afb50b0a48b76c7942f721748e69f9d2f6390
Buztas <114823431+Buztas@users.noreply.github.com>
2023-10-03 21:55:10 +0300

    Added POST and DELETE Endpoints

--- PineAPP/Controllers/DecksController.cs
+++ PineAPP/Controllers/DecksController.cs
@@ -1,8 +1,9 @@
-﻿using System.Net;
+﻿﻿using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PineAPP.Data;
 using PineAPP.Models;
+using PineAPP.Models.Dto;
 
 namespace PineAPP.Controllers;
 
@@ -49,7 +50,7 @@ public class DecksController : ControllerBase
         return Ok(response);
     }
     
-    [HttpGet("{deckId:int}")]
+    [HttpGet("{deckId:int}", Name = "GetDeck")]
     public async Task<ActionResult<ApiResponse<Deck>>> GetDeckById(int deckId)
     {
         var deck = await _db.Decks
@@ -59,5 +60,106 @@ public class DecksController : ControllerBase
         
         var response = new ApiResponse<Deck>(HttpStatusCode.OK, isSuccess: true, deck);
         return Ok(response);
+    } 
+    
+    [HttpPost("Add")]
+    public async Task<ActionResult<ApiResponse<List<Deck>>>> AddDeck([FromBody] CreateDeckDTO createDeckDto)
+    {
+    try
+    {
+        var response = new ApiResponse<List<Deck>>(
+        isSuccess: false,
+        statusCode: HttpStatusCode.BadRequest,
+        result: null, // You can specify the result as needed
+        errorMessage: "CreateDeckDTO is null.");
+
+        if (ModelState.IsValid)
+        {
+            if (createDeckDto == null)
+            {
+
+                return BadRequest(response);
+            }
+        }
+
+        Deck createNewDeck = new()
+        {
+            Name = createDeckDto.Name,
+            Description = createDeckDto.Description,
+            IsPersonal = createDeckDto.IsPersonal,
+            CreatorId = createDeckDto.CreatorId,
+        };
+
+        _db.Decks.Add(createNewDeck);
+        _db.SaveChanges();
+
+        response = new ApiResponse<List<Deck>>(
+        isSuccess: true,
+        statusCode: HttpStatusCode.Created,
+        result: new List<Deck> { createNewDeck },
+        errorMessage: null);
+
+        return CreatedAtRoute("GetDeck", new { deckId = createNewDeck.Id }, response);
+    }
+    catch (Exception e)
+    {
+        var response = new ApiResponse<List<Deck>>(
+            isSuccess: false,
+            statusCode: HttpStatusCode.InternalServerError,
+            result: null,
+            errorMessage: e.ToString());
+
+        return StatusCode((int)HttpStatusCode.InternalServerError, response);
     }
 }
+
+    [HttpDelete("Delete/{deckId:int}")]
+    public async Task<ActionResult<ApiResponse<List<Deck>>>> DeleteDeckById(int deckId)
+    {
+        try
+        {
+            var response = new ApiResponse<List<Deck>>(
+            isSuccess : false,
+            statusCode : HttpStatusCode.InternalServerError,
+            result : null,
+            errorMessage: "Deck id is 0, Deck does not exist/is invalid");
+
+            if (ModelState.IsValid)
+            {
+                Deck Deck = await _db.Decks.FindAsync(deckId);
+                if (deckId == 0)
+                {
+                    return BadRequest(response);
+                }
+
+                response = new ApiResponse<List<Deck>>(
+                isSuccess: true,
+                statusCode: HttpStatusCode.NoContent,
+                result: null,
+                errorMessage: null);
+
+                _db.Decks.Remove(Deck);
+                _db.SaveChanges();
+                return Ok(response);
+            }
+        }
+        catch (Exception e)
+        {
+            var response = new ApiResponse<List<Deck>>(
+            isSuccess: false,
+            statusCode: HttpStatusCode.InternalServerError,
+            result: null,
+            errorMessage: e.ToString());
+        
+            return BadRequest(response);
+        }
+
+        return NotFound();
+    }
+}
+
+//TO DO
+
+//Make it so when you are on the /create endpoint it redirects you to the new deck you are creating
+//Implement Delete functionality
+//Implement Add Cards functionality
