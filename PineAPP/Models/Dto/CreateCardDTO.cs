c57e395632c8580d20f83bb246f006a47f754b5e
Buztas <114823431+Buztas@users.noreply.github.com>
2023-10-04 16:35:20 +0300

    Added Card DTO

---
+++ PineAPP/Models/Dto/CreateCardDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PineAPP.Models.Dto;
+
+public class CreateCardDTO
+{
+    [Required]
+    [MaxLength(255)]
+    public string Front { get; set;}
+
+    [Required]
+    public string Back { get; set;}
+
+    public int DeckId {get; set;}
+    
+}
