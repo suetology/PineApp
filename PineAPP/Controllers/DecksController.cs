5d0bcf5d46f0a05c52f52ca4c49afefe59584927
Patriciakat <116752299+Patriciakat@users.noreply.github.com>
2023-10-12 17:13:23 +0300

    Update DecksController.cs

--- PineAPP/Controllers/DecksController.cs
+++ PineAPP/Controllers/DecksController.cs
@@ -342,11 +342,10 @@ public class DecksController : ControllerBase
         var totalCardsInDeck = await _db.Cards.Where(c => c.DeckId == card.DeckId).CountAsync();
         var currentCardIndex = await _db.Cards.Where(c => c.DeckId == card.DeckId && c.Id <= cardId).CountAsync();
 
-        // add these to your response model or use ViewBag (if using Razor views)
         card.TotalCardsInDeck = totalCardsInDeck;
         card.CurrentCardIndex = currentCardIndex;
 
-        return Ok(card); // Adjust this based on your ApiResponse structure
+        return Ok(card);
     }
 }
 
