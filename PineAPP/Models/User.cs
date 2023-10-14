ead3684d8a415ee0aac0282c3ed87e96de521699
MatasKvn <matas.kvainauskas@protonmail.com>
2023-10-14 16:35:37 +0300

    Created User class

---
+++ PineAPP/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PineAPP.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(320)]
+    public string Email { get; set; }
+    
+    [Required]
+    [MaxLength(32)]
+    public string Name { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string Password { get; set; }
+}
